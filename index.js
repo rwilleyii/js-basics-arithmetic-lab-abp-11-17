1286b6b59eb84cf9c81150335c9f46932d9e4309
rwilleyii <rwilley@me.com>
2017-11-12 23:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * 20;
 
-let randomInteger = Math.floor(randomNumber)
+let randomInteger = Math.floor(randomNumber);
+
+let randomUserID = randomInteger + 1000000000
